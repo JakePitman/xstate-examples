0b4e1b9fa9ede179c3da9629f6186de4889aff38
Jake Pitman <jakeseanp@gmail.com>
2024-10-23 00:37:36 +0900

    Add isOn guard to trafficLightMachine

--- src/app/_stateMachines/trafficLightMachine.ts
+++ src/app/_stateMachines/trafficLightMachine.ts
@@ -1,58 +1,66 @@
-import { setup, assign, createActor } from "xstate";
+import { assign, createActor, createMachine } from "xstate";
 
 type Context = {
   isOn: boolean;
 };
-export const trafficLightMachine = setup({
-  types: {
-    context: {} as Context,
-    events: {} as
-      | { type: "CHANGE" }
-      | { type: "TURN_OFF" }
-      | { type: "TURN_ON" }
-      | { type: "TOGGLE_ON_OFF" },
-  },
-}).createMachine({
-  /** @xstate-layout N4IgpgJg5mDOIC5QAoC2BDAxgCwJYDswBKAYgGEAJAQQDkBxAUQG0AGAXUVAAcB7WXAC64e+TiAAeiAEwA2AOwA6AJwAWFnJVyAzDICsWlioCMAGhABPaUsUAOKcaVLdUljvtyAvh7NoseQqQAKgCqAEo0APoA8gBiMawcSCC8-EIiYpIIskYKWs4uKvpGckpacrpmllksUgq6KqoyMloqMkosNlpePhg4BMQkIeHRNAliKYLCokmZsip1cjZGRjYqS0qrlYhG6gpSGtpaxS31RirdIL59AYNRdHQAMgwj0XFjSRNp06CzZQosLGsuichWKRikNi2CHBFyu-mICjooQYDBo5Go9GY7HGfEm6Rm210MgUKhcRPaRzkLBkKhUUPqtS0Nn0SjOwLa5VhvXhRAUAE0GA8HlEAOro2iMd7cXFfDKINQsBRnUGslzKyEWRAM3LMrSswpKDm6Ll+fq85EAEXFmKlyRlUzlCAVStpuiMqpY6qhpJy1lkR2sqikes8F3wPAgcDEcLNONSDoJCAAtKZNQgtC0FDTrCs1oDAU0TdcEUiUTQ43jvhJ5XJvfsFHIpFIVjYNC5DudvJduWb+YLhSKK7LExCtCTdJ0pM5OnrnPTSXV9DoamcgUYizyFJahwmflr6ysm+UyisMzJvQY9k33XIaQYM8avB4gA */
-  context: {
-    isOn: true,
-  },
-  initial: "green",
-  states: {
-    green: {
-      on: {
-        CHANGE: "yellow",
+export const trafficLightMachine = createMachine(
+  {
+    /** @xstate-layout N4IgpgJg5mDOIC5QAoC2BDAxgCwJYDswBKAYgGEAJAQQDkBxAUQG0AGAXUVAAcB7WXAC64e+TiAAeiAEwA2AOwA6AJwAWFnJVyAzDICsWlioCMAGhABPaUsUAOKcaVLdUljvtyAvh7NoseQqQAKgCqAEo0APoA8gBiMawcSCC8-EIiYpIIskYKWs4uKvpGckpacrpmllksUgq6KqoyMloqMkosNlpePhg4BMQkIeHRNAliKYLCokmZsip1cjZGRjYqS0qrlYhG6gpSGtpaxS31RirdIL59AYNRdHQAMgwj0XFjSRNp06CzZQosLGsuichWKRikNi2CHBFyu-mICjooQYDBo5Go9GY7HGfEm6Rm210MgUKhcRPaRzkLBkKhUUPqtS0Nn0SjOwLa5VhvXhRAUAE0GA8HlEAOro2iMd7cXFfDKINQsBRnUGslzKyEWRAM3LMrSswpKDm6Ll+fq85EAEXFmKlyRlUzlCAVStpuiMqpY6qhpJy1lkR2sqikes8F3wPAgcDEcLNONSDoJCAAtKZNQgtC0FDTrCs1oDAU0TdcEUiUTQ43jvhJ5XJvfsFHIpFIVjYNC5DudvJduWb+YLhSKK7LExCtCTdJ0pM5OnrnPTSXV9DoamcgUYizyFJahwmflr6ysm+UyisMzJvQY9k33XIaQYM8avB4gA */
+    types: {
+      context: {} as Context,
+      events: {} as
+        | { type: "CHANGE" }
+        | { type: "TURN_OFF" }
+        | { type: "TURN_ON" }
+        | { type: "TOGGLE_ON_OFF" },
+      guards: {} as { type: "isOn" },
+    },
+    context: {
+      isOn: true,
+    },
+    initial: "green",
+    states: {
+      green: {
+        on: {
+          // This will stop the transition to yellow when "isOn" guard returns false
+          CHANGE: { target: "yellow", guard: "isOn" },
+        },
+      },
+      yellow: {
+        on: {
+          CHANGE: { target: "red", guard: "isOn" },
+        },
+      },
+      red: {
+        on: {
+          CHANGE: { target: "green", guard: "isOn" },
+        },
       },
     },
-    yellow: {
-      on: {
-        CHANGE: "red",
+    on: {
+      CHANGE: {},
+      TURN_OFF: {
+        actions: assign({
+          isOn: false,
+        }),
       },
-    },
-    red: {
-      on: {
-        CHANGE: "green",
+      TURN_ON: {
+        actions: assign({
+          isOn: true,
+        }),
+      },
+      TOGGLE_ON_OFF: {
+        actions: assign({
+          isOn: ({ context }) => !context.isOn,
+        }),
       },
     },
   },
-  on: {
-    CHANGE: {},
-    TURN_OFF: {
-      actions: assign({
-        isOn: false,
-      }),
+  {
+    guards: {
+      isOn: ({ context }: { context: Context }) => context.isOn,
     },
-    TURN_ON: {
-      actions: assign({
-        isOn: true,
-      }),
-    },
-    TOGGLE_ON_OFF: {
-      actions: assign({
-        isOn: ({ context }) => !context.isOn,
-      }),
-    },
-  },
-});
+  }
+);
 
 export const trafficLightActor = createActor(trafficLightMachine).start();
